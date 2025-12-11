e9d6746670f1a557073eeefd0efab8a655c72e68
mhdfayiiz <muhammedfayiz.ec@gmail.com>
2025-12-11 10:34:11 +0530

    top module testbench created and verified

---
+++ testbenches/testbench.v
@@ -0,0 +1,94 @@
+`timescale 1ns/1ps
+
+module Top_tb;
+reg clk;
+reg reset;
+reg start;
+reg signed [7:0] A_in;
+reg signed [7:0] B_in;
+reg [2:0] opcode;
+
+wire signed [7:0] result_out;
+wire carry;
+wire zero_flag;
+wire overflow_flag;
+wire div_by_zero;
+wire done;
+
+Top dut(
+.clk(clk),.reset(reset),.start(start),.A_in(A_in),.B_in(B_in),.opcode(opcode),.result_out(result_out),.carry(carry),.zero_flag(zero_flag),.overflow_flag(overflow_flag),.div_by_zero(div_by_zero),.done(done));
+
+initial 
+begin
+$dumpfile("wave.vcd");   
+$dumpvars(0, Top_tb);    
+end
+
+
+
+always #5 clk = ~clk; 
+initial begin
+clk = 0;
+reset = 1;
+start = 0;
+A_in = 0;
+B_in = 0;
+opcode = 0;
+#20 reset = 0;
+$display("\n  TEST 1: ADD ");
+A_in = 8'd10;
+B_in = 8'd5;
+opcode = 3'b000;
+start = 1;
+#10 start = 0;
+wait(done);
+#2;
+$display("Time=%0dns | A=%0d | B=%0d | opcode=%b | result=%0d | carry=%b | zero=%b | overflow=%b | div0=%b | done=%b",$time, A_in, B_in, opcode, result_out, carry, zero_flag, overflow_flag, div_by_zero, done);
+ 
+
+$display("\n  TEST 2: SUB ");
+ A_in = 8'd20;
+ B_in = 8'd25;
+ opcode = 3'b001;
+ start = 1;
+ #10 start = 0;
+ wait(done);
+ #2;
+$display("Time=%0dns | A=%0d | B=%0d | opcode=%b | result=%0d | carry=%b | zero=%b | overflow=%b | div0=%b | done=%b",$time, A_in, B_in, opcode, result_out, carry, zero_flag, overflow_flag, div_by_zero, done);
+
+$display("\n   TEST 3: MUL  ");
+A_in = 8'd7;
+B_in = 8'd6;
+opcode = 3'b010;
+start = 1;
+#10 start = 0;
+wait(done);
+#2;
+$display("Time=%0dns | A=%0d | B=%0d | opcode=%b | result=%0d | carry=%b | zero=%b | overflow=%b | div0=%b | done=%b",$time, A_in, B_in, opcode, result_out, carry, zero_flag, overflow_flag, div_by_zero, done);
+
+$display("\n   TEST 4: DIV  ");
+A_in = 8'd50;
+B_in = 8'd10;
+opcode = 3'b011;
+start = 1;
+#10 start = 0;
+wait(done);
+#2;
+$display("Time=%0dns | A=%0d | B=%0d | opcode=%b | result=%0d | carry=%b | zero=%b | overflow=%b | div0=%b | done=%b",$time, A_in, B_in, opcode, result_out, carry, zero_flag, overflow_flag, div_by_zero, done);
+
+$display("\n  TEST 5: DIV BY ZERO ");
+A_in = 8'd100;
+B_in = 0;
+opcode = 3'b011; 
+start = 1;
+#10 start = 0;
+wait(done);
+#2;
+$display("Time=%0dns | A=%0d | B=%0d | opcode=%b | result=%0d | carry=%b | zero=%b | overflow=%b | div0=%b | done=%b",$time, A_in, B_in, opcode, result_out, carry, zero_flag, overflow_flag, div_by_zero, done);
+
+$display("\n ALL TESTS FINISHED \n");
+$finish;
+end
+
+
+endmodule
